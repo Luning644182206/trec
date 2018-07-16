f0df06855eeddc37589bc39fbd821841b251bc36
luning04 <luning04@baidu.com>
2018-07-16 22:34:34 +0800

    hotfix: bbc不能爬取的bug

--- src/class/search.py
+++ src/class/search.py
@@ -15,7 +15,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as expected
 from selenium.webdriver.support.wait import WebDriverWait
-class driverCommon:#定义一个类作为驱动
+# 定义一个类作为驱动
+class driverCommon:
     options = ''
     driver = ''
     wait = ''
@@ -23,7 +24,8 @@ class driverCommon:#定义一个类作为驱动
         self.options = Options()
         self.options.add_argument('-headless')  # 无头参数
         self.driver = Firefox(executable_path='./third_party/geckodriver', firefox_options = self.options)  # 配了环境变量第一个参数就可以省了，不然传绝对路径
-        self.wait = WebDriverWait(self.driver, timeout = 15)#设置成15
+        self.wait = WebDriverWait(self.driver, timeout = 15) # 设置成15
+
 class NewsSearch:
     keyWords = ''
     baseUrl = ''
@@ -75,19 +77,13 @@ class NewsSearch:
         searchUrl = self.baseUrl + querys + urlcode[website]+startNum
         return searchUrl
 
-    '''
-    foxNewsSearch:     foxNews爬虫
-    Input:             none
-    Output:            none
-    others:            none
-    '''
-   
-    def bbcNewsSearch(self):#搜索bbc新闻
-        bbcDriver = driverCommon()#生成一个driver对象
+    
+    def bbcNewsSearch(self): # 搜索bbc新闻
+        bbcDriver = driverCommon() # 生成一个driver对象
         for index in range(int(self.times)):
             searchUrl = self.createURL(index,self.website)
             bbcDriver.driver.get(searchUrl)
-            bbcDriver.wait.until(expected.visibility_of_element_located((By.CLASS_NAME, 'num-found')))
+            # bbcDriver.wait.until(expected.visibility_of_element_located((By.CLASS_NAME, 'num-found')))
             soup = BS(bbcDriver.driver.page_source, 'html.parser')
             # 找到每条新闻块
             news = soup.findAll(class_= 'has_image media-text')
@@ -110,10 +106,17 @@ class NewsSearch:
                 if (result['url'] != ''):
                     self.results.append(result)
         bbcDriver.driver.quit()
-    def foxNewsSearch(self):#搜索火狐的新闻
+
+    '''
+    foxNewsSearch:     foxNews爬虫
+    Input:             none
+    Output:            none
+    others:            none
+    '''    
+    def foxNewsSearch(self): # 搜索火狐的新闻
         foxDriver = driverCommon()
         for index in range(int(self.times)):
-            searchUrl = self.createURL(index,self.website)
+            searchUrl = self.createURL(index, self.website)
             foxDriver.driver.get(searchUrl)
             foxDriver.wait.until(expected.visibility_of_element_located((By.CLASS_NAME, 'num-found')))
             soup = BS(foxDriver.driver.page_source, 'html.parser')
